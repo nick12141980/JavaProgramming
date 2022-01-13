121af827ab0098ee8505533cd39a174e18cbdc23
Nick <nikolayshilov@yahoo.com>
2022-01-13 11:46:25 -0500

    day 38 Inheritance

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry",2020,20000, "White", 12500);
+
+        Tesla tesla = new Tesla("Model 3", 2021,55000,"Black",8000);
+
+        BMW bmw = new BMW("X5",2019,45000,"Gray",132000);
+
+    }
+}
