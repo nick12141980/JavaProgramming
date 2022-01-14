a816d1fae1e70abed13b9b8fce9966bdf658a8ca
Nick <nikolayshilov@yahoo.com>
2022-01-14 11:42:49 -0500

    day 39 shape task

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -1,6 +1,37 @@
 package day39_Recap.shapeTask;
 
-public class Rectangle {
+public class Rectangle extends Shape{
 
+    public Rectangle(String name, double length, double width) {
+        super(name);
+        setLength(length);
+        setWidth(width);
+    }
 
+    private double length;
+    private double width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length <= 0){
+            System.err.println("Invalid length");
+            System.exit(1);
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width<=0){
+            System.err.println("Invalid width");
+            System.exit(1);
+        }
+        this.width = width;
+    }
 }
