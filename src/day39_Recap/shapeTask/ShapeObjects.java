8277243753f7139fca9f049b4611018e87f39b05
Nick <nikolayshilov@yahoo.com>
2022-01-14 14:46:59 -0500

    day 39 shape task

--- src/day39_Recap/shapeTask/ShapeObjects.java
+++ src/day39_Recap/shapeTask/ShapeObjects.java
@@ -10,5 +10,22 @@ public class ShapeObjects {
         square.setSide(20);
         System.out.println(square);
 
+        System.out.println("-----------------------------");
+
+        Rectangle rectangle = new Rectangle(10,20);
+        rectangle.setWidth(10);
+        rectangle.setLength(30);
+        System.out.println(rectangle);
+        System.out.println(rectangle.getLength());
+
+        System.out.println(rectangle.getName());
+
+        System.out.println("-------------------------------");
+
+        Circle circle = new Circle(11);
+        System.out.println(circle);
+        System.out.println(circle.pi);
+
+
     }
 }
