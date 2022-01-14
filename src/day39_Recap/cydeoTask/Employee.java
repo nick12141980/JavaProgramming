ec9d53eda068dc38796ba3eebdfd5c9aeb00bc34
Nick <nikolayshilov@yahoo.com>
2022-01-14 15:43:02 -0500

    day 39 cydeo task

---
+++ src/day39_Recap/cydeoTask/Employee.java
@@ -0,0 +1,32 @@
+package day39_Recap.cydeoTask;
+
+public class Employee {
+
+    private int employeeID;
+    private String jobTitle;
+    private double salary;
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
