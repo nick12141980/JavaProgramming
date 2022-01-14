685079b6b7ae4b3bc8510599439311c0a8d8abee
Nick <nikolayshilov@yahoo.com>
2022-01-14 14:28:52 -0500

    day 39 shape task

--- src/day39_Recap/shapeTask/ShapeObjects.java
+++ src/day39_Recap/shapeTask/ShapeObjects.java
@@ -4,5 +4,11 @@ public class ShapeObjects {
 
     public static void main(String[] args) {
 
+        Square square = new Square(11);
+        System.out.println(square);
+
+        square.setSide(20);
+        System.out.println(square);
+
     }
 }
