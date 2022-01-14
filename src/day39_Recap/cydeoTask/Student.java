6e31ba1cf7dfa43aa8edb23c5c865641d05d5b2f
Nick <nikolayshilov@yahoo.com>
2022-01-14 16:12:22 -0500

    day 39 cydeo task

---
+++ src/day39_Recap/cydeoTask/Student.java
@@ -0,0 +1,23 @@
+package day39_Recap.cydeoTask;
+
+public class Student {
+
+    private int studentID;
+    private String fieldOfStudy;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+}
