b4cb48a6c038e5fada39a4ac4d132f5c2fba1824
Nick <nikolayshilov@yahoo.com>
2022-01-20 10:49:35 -0500

    day 40 ProtectedAccessModifier

--- src/day40_FinalKeyword/ProtectedAccessModifier.java
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -2,6 +2,6 @@ package day40_FinalKeyword;
 
 public class ProtectedAccessModifier {
 
-    static String name1 = "Cydeo";
-    protected static String name2 = "WoodenSpoon";
+    static String name1 = "Cydeo"; //default
+    protected static String name2 = "WoodenSpoon"; //protected
 }
---
+++ src/utilities/AccessModifiersTest3.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiersTest3 {
+
+    public static void main(String[] args) {
+
+        //System.out.println(ProtectedAccessModifier.name1); not visible outside a package
+    }
+}
