de6571c5b0947d085c494f43f281d496d7ad6255
Nick <nikolayshilov@yahoo.com>
2022-01-20 10:58:59 -0500

    day 40 ProtectedAccessModifier

--- src/utilities/AccessModifiersTest4.java
+++ src/utilities/AccessModifiersTest4.java
@@ -7,7 +7,7 @@ public class AccessModifiersTest4 extends ProtectedAccessModifier {
     public static void main(String[] args) {
 
       //  System.out.println(AccessModifiersTest4.name1); default is not visible outside the package
-        System.out.println(AccessModifiersTest4.name2); // protected is visible
+        System.out.println(AccessModifiersTest4.name2); // protected is visible in subclass
 
         AccessModifiersTest4.method2();
 
