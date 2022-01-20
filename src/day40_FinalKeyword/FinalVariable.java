9a5eeb0c0511d33e90c4d81e8b6b519a63b366a5
Nick <nikolayshilov@yahoo.com>
2022-01-20 11:53:29 -0500

    day 40 Final keyword

--- src/day40_FinalKeyword/FinalVariable.java
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -2,9 +2,19 @@ package day40_FinalKeyword;
 
 public class FinalVariable {
 
+    final String birthDay;
+
+    public FinalVariable(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
     public static void main(String[] args) {
 
-        double pi = 3.14;
+        final double pi = 3.14;
+        final  String name;
+        name = "Java";
+
+
 
     }
 }
