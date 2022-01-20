83a2cff7e0853edc8ecd5ee6f91de130173bf685
Nick <nikolayshilov@yahoo.com>
2022-01-20 10:51:47 -0500

    day 40 ProtectedAccessModifier

--- src/utilities/AccessModifiersTest3.java
+++ src/utilities/AccessModifiersTest3.java
@@ -7,5 +7,6 @@ public class AccessModifiersTest3 {
     public static void main(String[] args) {
 
         //System.out.println(ProtectedAccessModifier.name1); not visible outside a package
+        //System.out.println(ProtectedAccessModifier.name2); not always visible outside a package
     }
 }
