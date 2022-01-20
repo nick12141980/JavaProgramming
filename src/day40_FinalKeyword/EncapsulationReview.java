00ae004cb18cadf1699463a53b783ea709c65a55
Nick <nikolayshilov@yahoo.com>
2022-01-20 10:24:05 -0500

    day 40 encapsulation review

--- src/day40_FinalKeyword/EncapsulationReview.java
+++ src/day40_FinalKeyword/EncapsulationReview.java
@@ -1,10 +1,20 @@
 package day40_FinalKeyword;
 
 import day39_Recap.shapeTask.Circle;
+import day39_Recap.shapeTask.Square;
 
 public class EncapsulationReview {
 
     private Circle circle;
+    private Square square;
+
+    public Square getSquare() {
+        return square;
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
 
     public Circle getCircle(){
         return circle;
