03dc295093cf795a81705672300a3430bfdc42c6
Nick <nikolayshilov@yahoo.com>
2022-01-21 10:31:53 -0500

    day 41 Exceptions

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,18 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+
+    public static void main(String[] args) {
+
+        //unchecked
+
+        int a = 10;
+        int b = 0;
+
+        System.out.println(a / b);
+
+
+    }
+
+
+}
