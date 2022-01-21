c46ea46abb96a381724cafc5198709ef48d44723
Nick <nikolayshilov@yahoo.com>
2022-01-21 10:58:45 -0500

    day 41 Exceptions

--- src/day41_Exceptions/Checked_vs_Unchecked.java
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -1,5 +1,10 @@
 package day41_Exceptions;
 
+import day39_Recap.cydeoTask.Student;
+
+import java.io.FileInputStream;
+import java.util.Locale;
+
 public class Checked_vs_Unchecked {
 
     public static void main(String[] args) {
@@ -9,7 +14,33 @@ public class Checked_vs_Unchecked {
         int a = 10;
         int b = 0;
 
-        System.out.println(a / b);
+       // System.out.println(a / b);
+
+        char[] characters = {'a','b','c'};
+     //  System.out.println(characters[99]);
+
+        Student student = null;
+
+     //   System.out.println(student.getName());
+       // student.study();
+
+        String str = "Wooden Spoon";
+
+      //  str = null;
+
+        System.out.println(str.toUpperCase());
+
+        //checked
+
+        System.out.println("Hello");
+
+        //Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+
+       // FileInputStream file = new FileInputStream("path");
+
+
 
 
     }
