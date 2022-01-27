910eabb9ae838dabd03609aea1265c0e4c386500
Nick <nikolayshilov@yahoo.com>
2022-01-27 12:51:02 -0500

    day 42 Exceptions

---
+++ src/day42_Exceptions/CustomExceptions.java
@@ -0,0 +1,15 @@
+package day42_Exceptions;
+
+class NickException extends RuntimeException{
+    public NickException(String message){
+       super(message);
+    }
+}
+
+public class CustomExceptions {
+
+    public static void main(String[] args) {
+        throw new NickException("This is my exception");
+    }
+
+}
