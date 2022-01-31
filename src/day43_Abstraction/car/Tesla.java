fbd03908e8cae250b87a04ada2cd12e9fa87d4f9
Nick <nikolayshilov@yahoo.com>
2022-01-31 14:43:04 -0500

    Abstraction

--- src/day43_Abstraction/car/Tesla.java
+++ src/day43_Abstraction/car/Tesla.java
@@ -1,4 +1,18 @@
 package day43_Abstraction.car;
 
-public class Tesla {
+public final class Tesla extends Car{
+
+
+    public Tesla( String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Say start");
+    }
+
+    public void autoPilot(){
+        System.out.println(getMake()+" has autopilot feature");
+    }
 }
