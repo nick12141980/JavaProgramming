3f2a032530c153ec4a37926e7824ae779c384b5a
Nick <nikolayshilov@yahoo.com>
2022-02-03 12:05:30 -0500

    day 44 Abstraction

---
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -0,0 +1,17 @@
+package day44_Abstraction.animalTask;
+
+public class Dog extends Animal{
+
+    public Dog(String name, String size, char gender, String breed, String color, int age) {
+        super(name, size, gender, breed, color, age);
+    }
+
+    public void bark(){
+        System.out.println(getName()+" is barking");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" eats pizza");
+    }
+}
