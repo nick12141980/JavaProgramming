977108efaba3552924496d6d67342a73da924ab1
Nick <nikolayshilov@yahoo.com>
2022-02-07 15:30:53 -0500

    Shape Task

--- src/day45_Abstraction/shape/Square.java
+++ src/day45_Abstraction/shape/Square.java
@@ -1,8 +1,39 @@
 package day45_Abstraction.shape;
 
-public class Square {
+public class Square extends Shape{
 
     private double side;
 
+    public Square(String name) {
+        super(name);
+    }
 
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side<=0){
+            throw new RuntimeException("Invalid side "+ side);
+        }
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side*side;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                super.toString()+
+                "side=" + side +
+                '}';
+    }
 }
