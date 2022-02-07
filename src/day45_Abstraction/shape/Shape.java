69d2e5d602e288b6e6e9d2980bc226c709a1a708
Nick <nikolayshilov@yahoo.com>
2022-02-07 14:49:04 -0500

    Shape Task

---
+++ src/day45_Abstraction/shape/Shape.java
@@ -0,0 +1,19 @@
+package day45_Abstraction.shape;
+
+public abstract class Shape {
+
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public abstract double area();
+
+    public abstract double perimeter();
+}
