5aee3260a5f25e21a98da54bb279121cff68b299
Nick <nikolayshilov@yahoo.com>
2022-02-10 11:41:28 -0500

    Polymorphism

---
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -0,0 +1,19 @@
+package day46_Polymorphism;
+
+import day45_Abstraction.shape.Circle;
+import day45_Abstraction.shape.Square;
+
+public class PolymorphismIntro {
+
+    String str = "Wooden Spoon";
+
+    Integer n1 = 2;
+
+    Double b2 = 5.5;
+
+    Boolean r1 =true;
+
+    Object[] = {str, n1, b2, r1, new Circle(4), new Square(5)};
+
+
+}
