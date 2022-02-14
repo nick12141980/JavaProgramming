7497f576908d822428027e280d73c4ca6daff865
Nick <nikolayshilov@yahoo.com>
2022-02-14 12:36:24 -0500

    Polymorphism

--- src/day47_Polymorphism/ReferenceTypeCastings.java
+++ src/day47_Polymorphism/ReferenceTypeCastings.java
@@ -1,9 +1,11 @@
 package day47_Polymorphism;
 
 import day44_Abstraction.animalTask.Animal;
+import day44_Abstraction.animalTask.Cat;
 import day44_Abstraction.animalTask.Dog;
 import day45_Abstraction.shape.Circle;
 import day45_Abstraction.shape.Shape;
+import day45_Abstraction.shape.Square;
 
 public class ReferenceTypeCastings {
 
@@ -20,6 +22,15 @@ public class ReferenceTypeCastings {
         System.out.println(dog.getName());
 
         dog.bark();
+
+        Shape shape1 = new Square(2);
+
+        System.out.println( ((Square)shape1).getSide() );
+
+        Animal animal2 = new Cat("Max", "M",'M',"Bengal","White",4);
+
+        ( (Cat) animal2).meow();
+       //cat.meow();
 
 
     }
