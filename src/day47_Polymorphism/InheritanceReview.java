8f02f1a2c0591aa1bfef756163129f55d9757442
Nick <nikolayshilov@yahoo.com>
2022-02-14 15:08:41 -0500

    OOP Recap

---
+++ src/day47_Polymorphism/InheritanceReview.java
@@ -0,0 +1,42 @@
+package day47_Polymorphism;
+
+public class InheritanceReview {
+
+    public InheritanceReview(int a){
+        System.out.println("Parent class constructor");
+    }
+
+    int a;
+    void method1{
+
+    }
+
+}
+
+class A extends InheritanceReview{
+
+    public A(int a) {
+        super(a);
+    }
+}
+
+class B  extends InheritanceReview{
+
+    public B(int a) {
+        super(a);
+    }
+}
+
+class C extends  InheritanceReview{
+
+    public C(int a) {
+        super(a);
+    }
+}
+
+class Aa extends A{
+
+    public Aa(int a) {
+        super(a);
+    }
+}
