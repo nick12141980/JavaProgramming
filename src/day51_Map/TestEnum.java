75fd2f82363b0e84ba837ebcec197d6288105f72
Nick <nikolayshilov@yahoo.com>
2022-03-01 16:49:15 -0500

    ENUM

---
+++ src/day51_Map/TestEnum.java
@@ -0,0 +1,27 @@
+package day51_Map;
+
+public class TestEnum {
+    public static void main(String[] args) {
+
+        //String browser = "Wooden Spoon";
+
+        Browsers browser = Browsers.CHROME;
+
+        switch (browser){
+            case FIREFOX:
+                System.out.println("Firefox is selected");
+                break;
+            case OPERA:
+                System.out.println("Opera");
+                break;
+            case EDGE:
+                System.out.println("Edge");
+                break;
+            default:
+                System.out.println("Chrome");
+        }
+    }
+
+
+
+}
