e4af9c0a3e6d36f837ea6eb7b597a133c77ccf5f
Nick <nikolayshilov@yahoo.com>
2022-03-07 11:47:22 -0500

    Functional interface

--- src/day53_FunctionalInterface/BuiltInFunctionalInterfaces1.java
+++ src/day53_FunctionalInterface/BuiltInFunctionalInterfaces1.java
@@ -3,6 +3,7 @@ package day53_FunctionalInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class BuiltInFunctionalInterfaces1 {
@@ -34,6 +35,25 @@ public class BuiltInFunctionalInterfaces1 {
 
         System.out.println(names);
 
+        System.out.println("------------------------------------------");
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+            }
+        };
+
+        printEach.accept("Wooden Spoon");
+
+        System.out.println("------------------------------------------");
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,34,5,6,7,8,9,0));
+      //  for (Integer each : list2) {
+       //     if (each%2 != 0){
+         //      System.out.println(each);
+          //  }
+
+        list2.forEach(p -> {if (p%2 != 0) System.out.println(p);});
 
 
     }
