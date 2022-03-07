c8df7348588e372e9e02a89204940af5a0991125
Nick <nikolayshilov@yahoo.com>
2022-03-07 15:32:29 -0500

    Stream

--- src/day53_FunctionalInterface/StreamPractice.java
+++ src/day53_FunctionalInterface/StreamPractice.java
@@ -25,6 +25,15 @@ public class StreamPractice {
         nums2 = Arrays.stream(nums2).skip(4).toArray();
         System.out.println(Arrays.toString(nums2));
 
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        list3 = list3.stream().limit(7).collect(Collectors.toList());
+        System.out.println(list3);
+
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        list4 = list4.stream().map( p -> p*10 ).collect(Collectors.toList());
+        System.out.println(list4);
+
+
     }
 
 }
